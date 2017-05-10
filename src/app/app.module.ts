773b42f9100d0b43eb05072df6b83f8c1468f026
Ramazan Ayvaz <ramazanayvaz@msn.com>
2017-05-10 16:54:34 +0300

    Header, events and footer components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EventsComponent,
+    FooterComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
